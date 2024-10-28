c7f2d814806f5b5279178efd9b4435f4f67e3bb8
mzamouneisi <mourad.zamoun@eisi-consulting.fr>
2024-10-28 16:07:52 +0100

    list url_api

--- src/app/service/Aservice.ts
+++ src/app/service/Aservice.ts
@@ -2,11 +2,11 @@
 
 export class Aservice {
     
-    private API_URL_LOC = 'http://localhost:8888/antennes/api/';
-    private API_URL_RENDER = 'https://antennes.onrender.com/antennes/api/';
-    private API_URL_AZURE = 'https://antennes-g2b2hrgnfvdpcaba.canadacentral-01.azurewebsites.net/antennes/api/';
+     API_URL_LOC = 'http://localhost:8888/antennes/api/';
+     API_URL_RENDER = 'https://antennes.onrender.com/antennes/api/';
+     API_URL_AZURE = 'https://antennes.azurewebsites.net/antennes/api/';
 
 
-    protected API_URL = this.API_URL_AZURE;
+    public API_URL = this.API_URL_AZURE;
 
 }
--- src/app/service/share-service.ts
+++ src/app/service/share-service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 
 import { MatDialog } from '@angular/material/dialog';
 import { InfoDialogComponent } from '../compo/util/info-dialog/info-dialog.component';
+import { Aservice } from './Aservice';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UtilService {
+export class UtilService extends Aservice {
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog) {
+    super();
+  }
 
   showInfo(message: string): void {
     this.dialog.open(InfoDialogComponent, {
